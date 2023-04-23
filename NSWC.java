fa1f04d44c05b30d9a67c6fc7273f1bccda4eeb2
Lateef Quadri Olayinka <Lateef9816@gmail.com>
2023-04-23 17:44:35 +0100

    Update NSWC.java

--- NSWC.java
+++ NSWC.java
@@ -1,45 +1,59 @@
+// Java program to demonstrate working of Arrays.toString()
 import java.io.*;
 import java.util.*;
 
-public class NSWC_Citizen {
+// 1. Accept the beneficiary count +++
+// 2. Collect the marital status of each beneficiary 
+//    "Single" - 500, "Married" - 2000, "Seperated" - 3000, "Divorced" - 4000.
+// 3. Compute allowance for each beneficiary
+// 4. Display individual allowance with their status
+// 5. Display the total allowances paid.
 
-
-  public static void main(String[] args){
-    int numOfCitizens;
-    int totalAllowances = 0;
-    
-    
+public class NSWCAllowance {
+  
+  public static void main (String[] args) {
     Scanner input = new Scanner(System.in);
     
-    System.out.print("How many citizens? ");
-    numOfCitizens = input.nextInt();
+    // Accept beneficiary count
+    System.out.println("Enter beneficiary count: ");
+    int beneficiaryCount = input.nextInt();
+    int totalAllowances = 0;
     
-    String[] citizensStatus = new String[numOfCitizens];
-    
-    for(int i = 0; i < numOfCitizens; i++){
-      System.out.println("Enter marital status for citizen #" + i + ":");
-      citizensStatus[i] = input.nextLine();
+  
+    // TODO: Validate beneficiary count
+    while(beneficiaryCount < 1){
+      System.out.println("Enter beneficiary count (greater than zero): ");
+      beneficiaryCount = input.nextInt();
     }
     
+    // Collect and Store beneficiaries' marital status
+    String[] citizensMaritalStatus = new String[beneficiaryCount];
+    int[] citizensAllowances = new int[beneficiaryCount];
     
-    for(int i = 0; i < numOfCitizens; i++){
-      System.out.println("Enter marital status for citizen #" + i + ":");
+    for(int i = 0; i < beneficiaryCount; i++){
+      System.out.println("Enter the marital status of citizen #" + (i+1));
+      String maritalStatus = input.nextLine();
+  
+      int citizenAllowance = calculateAllowance(maritalStatus);
       
-      int allowance = calculateAllowance(citizensStatus[i]);
-      System.out.println("NSWC allowance for citizen #" + i + " of marital status " + citizensStatus[i] + " is N" + allowance);
-      
-      
-      totalAllowances += allowance;
+      // Update the total allowances
+      totalAllowances += citizenAllowance;
+
+      citizensMaritalStatus[i] = maritalStatus;
+      citizensAllowances[i] = citizenAllowance;
     }
     
+    for(int i = 0; i < beneficiaryCount; i++){
+      System.out.println("Citizen #" + (i+1) + " of " + citizensMaritalStatus[i] + " marital status is awarded N" + citizensAllowances[i] + ".");
+    }
     
-    
-    System.out.println(Arrays.toString(citizensStatus));
+    System.out.println("Total allowances paid is N" + totalAllowances);
     
   }
-
-  private static int calculateAllowance(String status){
+  
+  static int calculateAllowance(String status){
     int allowance;
+    
     switch(status){
       case "Single":
         allowance = 500;
