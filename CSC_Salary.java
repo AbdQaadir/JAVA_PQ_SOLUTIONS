4985439862301a5c7cadf49c5d6584d3e9bdfbd6
Lateef Quadri Olayinka <Lateef9816@gmail.com>
2023-04-23 18:40:10 +0100

    Create CSC_Salary.java

--- CSC_Salary.java
+++ CSC_Salary.java
@@ -1,70 +1,81 @@
-// Java program to demonstrate working of Arrays.toString()
-import java.io.*;
 import java.util.*;
 
-public class CSC_Salary {
+// 1. Accept name and Salaries of 20 academic staff
+// 2. Display a list containing name, salaries and the rank.
+// 3. Display the total and the average salary.
+// Professor N3000 - N5000
+// Associate Professor N2500 - N2999
+// Senior Lecturer N2000 - N2499
+// Lecturer N1000 - N1999
 
-  public static void main(String[] args){
-    int numOfCitizens;
-    int totalAllowances = 0;
-    
+public class AcademicStaff {
+  
+  public static void main (String[] args) {
+    int staffCount = 5;
+    String[] staffNames = new String[staffCount];
+    int[] staffSalaries = new int[staffCount];
     
     Scanner input = new Scanner(System.in);
     
-    System.out.print("How many citizens? ");
-    numOfCitizens = input.nextInt();
-    
-    String[] citizensStatus = new String[numOfCitizens];
-    
-    for(int i = 0; i < numOfCitizens; i++){
-      System.out.println("Enter marital status for citizen #" + i + ":");
-      citizensStatus[i] = input.nextLine();
+    for (int i = 0; i < staffCount; i++){
+      System.out.println("Enter name for staff #" + (i+1));
+      String name = input.nextLine();
+      staffNames[i] = name;
+      
+      System.out.println("Enter salary for staff #" + (i+1));
+      int salary = input.nextInt(); // 1000 + (500 * i); 
+      staffSalaries[i] = salary;
     }
     
-    
-    for(int i = 0; i < numOfCitizens; i++){
-      System.out.println("Enter marital status for citizen #" + i + ":");
+    for (int i = 0; i < staffCount; i++){
+      String staffName = staffNames[i];
+      int staffSalary = staffSalaries[i];
+      String staffRank = getStaffRank(staffSalary);
       
-      int allowance = calculateAllowance(citizensStatus[i]);
-      System.out.println("NSWC allowance for citizen #" + i + " of marital status " + citizensStatus[i] + " is N" + allowance);
-      
-      
-      totalAllowances += allowance;
+      System.out.println(staffName + " - N" + staffSalary + " - " + staffRank);
     }
     
+    int totalSalary = calculateTotal(staffSalaries);
+    int averageSalary = calculateAverage(totalSalary, staffCount);
     
-    
-    System.out.println(Arrays.toString(citizensStatus));
+    System.out.println("Average Salary: N" + averageSalary);
+    System.out.println("Total Salary: N" + totalSalary);
     
   }
 
-  private static int calculateAllowance(String status){
-    int allowance;
-    switch(status){
-      case "Single":
-        allowance = 500;
-        break;
-      case "Married":
-        allowance = 2000;
-        break;
-      case "Seperated":
-        allowance = 3000;
-        break;
-      case "Divorced":
-        allowance = 4000;
-        break;
-      default:
-        allowance = 0;
+  
+  static int calculateTotal(int[] staffSalaries){
+    int total = 0;
+    for (int i = 0; i < staffSalaries.length; i++) {
+      total += staffSalaries[i];
     }
     
-    return allowance;
+    return total;
+  }
+  
+  static int calculateAverage(int total, int staffCount){
+    int average = total / staffCount;
+    
+    return average;
+  }
+  
+  
+  static String getStaffRank(int salary){
+    String rank = "";
+    
+    if(salary >= 1000 && salary <= 1999){
+      rank = "Lecturer";
+    }
+    else if(salary >= 2000 && salary <= 2499){
+      rank = "Senior Lecturer";
+    }
+    else if(salary >= 2500 && salary <= 2999){
+      rank = "Associate Professor";
+    }
+    else if(salary >= 3000 && salary <= 5000){
+      rank = "Professor";
+    }
+    
+    return rank;
   }
 }
-
-
-
-
-
-
-
-
