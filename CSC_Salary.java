8e98509247aff78e1d3cacbf3aa0cf6d9c111fbf
Lateef Quadri Olayinka <Lateef9816@gmail.com>
2023-04-23 17:03:55 +0100

    Create CSC_Salary.java

---
+++ CSC_Salary.java
@@ -0,0 +1,70 @@
+// Java program to demonstrate working of Arrays.toString()
+import java.io.*;
+import java.util.*;
+
+public class CSC_Salary {
+
+  public static void main(String[] args){
+    int numOfCitizens;
+    int totalAllowances = 0;
+    
+    
+    Scanner input = new Scanner(System.in);
+    
+    System.out.print("How many citizens? ");
+    numOfCitizens = input.nextInt();
+    
+    String[] citizensStatus = new String[numOfCitizens];
+    
+    for(int i = 0; i < numOfCitizens; i++){
+      System.out.println("Enter marital status for citizen #" + i + ":");
+      citizensStatus[i] = input.nextLine();
+    }
+    
+    
+    for(int i = 0; i < numOfCitizens; i++){
+      System.out.println("Enter marital status for citizen #" + i + ":");
+      
+      int allowance = calculateAllowance(citizensStatus[i]);
+      System.out.println("NSWC allowance for citizen #" + i + " of marital status " + citizensStatus[i] + " is N" + allowance);
+      
+      
+      totalAllowances += allowance;
+    }
+    
+    
+    
+    System.out.println(Arrays.toString(citizensStatus));
+    
+  }
+
+  private static int calculateAllowance(String status){
+    int allowance;
+    switch(status){
+      case "Single":
+        allowance = 500;
+        break;
+      case "Married":
+        allowance = 2000;
+        break;
+      case "Seperated":
+        allowance = 3000;
+        break;
+      case "Divorced":
+        allowance = 4000;
+        break;
+      default:
+        allowance = 0;
+    }
+    
+    return allowance;
+  }
+}
+
+
+
+
+
+
+
+
